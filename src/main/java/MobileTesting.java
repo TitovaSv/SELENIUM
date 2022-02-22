7271d71e8ff7bfc8868f5dfdd304620b920e8d9b
Svitlana Titova <titovasv@i.ua>
2022-02-22 13:04:32 +0200

    add changes

--- src/main/java/MobileTesting.java
+++ src/main/java/MobileTesting.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MobileTesting {
     @Test
-    public void checkMobilepayment(){
+    public void checkMobilepayment() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
@@ -23,17 +24,25 @@ public class MobileTesting {
         driver.findElement(expiredebitSource).sendKeys("0622");
         By cvvdebitSource = By.xpath("//input[@data-qa-node='cvvdebitSource']");
         driver.findElement(cvvdebitSource).sendKeys("494");
+        Thread.sleep(1500);
         By firstNamedebitSource = By.xpath("//input[@data-qa-node='firstNamedebitSource']");
         driver.findElement(firstNamedebitSource).sendKeys("Ronald");
         By lastNamedebitSource = By.xpath("//input[@data-qa-node='lastNamedebitSource']");
         driver.findElement(lastNamedebitSource).sendKeys("Clark");
         By submit = By.xpath("//button[@data-qa-node='submit']");
         driver.findElement(submit).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'Сплатити')]")).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'Назад')]")).click();
+
+        By operator = By.xpath("//span[@data-qa-node='nameB']");
 
 
-
-
-
+        Assert.assertEquals("Kyivstar Ukraine",driver.findElement(operator).getText());
+        Assert.assertEquals("4093 **** **** 1961",driver.findElement(By.xpath("//td[@data-qa-node='card']")).getText());
+        Assert.assertEquals("200 UAH",driver.findElement(By.xpath("//div[@data-qa-node='amount']")).getText());
+        Assert.assertEquals(" UAH",driver.findElement(By.xpath("//span[@data-qa-node='commission-currency']")).getText());
+        Assert.assertEquals("Поповнення мобільного",driver.findElement(By.xpath("//div[@data-qa-node='category']")).getText());
+        Assert.assertEquals("Поповнення телефону. На номер +380962142149",driver.findElement(By.xpath("//div[@data-qa-node='details']")).getText());
 
 
     }
